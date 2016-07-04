5ed68cd1dd426e3c07d687025c1fc4e33fc45937
Pierre Fernique <pfernique@gmail.com>
2016-07-04 13:17:29 +0200

    Update bld.bat

--- conda/libclang/bld.bat
+++ conda/libclang/bld.bat
@@ -8,13 +8,6 @@ set "srcdir=%cd%"
 cd ..
 mkdir build
 cd build
-cmake -G "Visual Studio 12" -DBUILD_SHARED_LIBS=1 \
-                            -DLLVM_ENABLE_RTTI=1 \
-                            -DCMAKE_INSTALL_PREFIX=%PREFIX% \
-                            -DCMAKE_BUILD_TYPE=Release \
-                            -DCMAKE_C_COMPILER=%PREFIX%/bin/gcc \
-                            -DCMAKE_CXX_COMPILER=%PREFIX%/bin/g++ \
-                            -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,%PREFIX%/lib -L%PREFIX%/lib" \
-                            %srcdir%
+cmake -G "Visual Studio 12" -DBUILD_SHARED_LIBS=1 -DLLVM_ENABLE_RTTI=1 -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=%PREFIX%/bin/gcc -DCMAKE_CXX_COMPILER=%PREFIX%/bin/g++ -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,%PREFIX%/lib -L%PREFIX%/lib" %srcdir%
 make
 make install
