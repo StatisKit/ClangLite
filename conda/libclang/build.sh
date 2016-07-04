bc17886d39f4a6db04a00cca0e25bee8e8b08897
Pierre Fernique <pfernique@gmail.com>
2016-07-04 10:43:32 +0200

    Update .travis.yml

--- conda/libclang/build.sh
+++ conda/libclang/build.sh
@@ -1,6 +1,8 @@
 #if [ "$(uname)" == "Linux" ]; then
 #    sudo apt-get install cmake zlib1g-dev scons 
 #fi
+strings ~/.miniconda2/envs/traviscii/lib/libstdc++.so.6 | grep GLIBCXX
+strings ${PREFIX}/lib/libstdc++.so.6 | grep GLIBCXX
 CC=${PREFIX}/bin/gcc
 CXX=${PREFIX}/bin/g++
 git checkout release_38
@@ -13,6 +15,6 @@ srcdir=`pwd`
 cd ..
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=1 -DLLVM_ENABLE_RTTI=1 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ $srcdir
+cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=1 -DLLVM_ENABLE_RTTI=1 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release $srcdir
 make
 make install
