0619be06ec2ba2f0ce768db93143448630e8937c
Pierre Fernique <pfernique@gmail.com>
2016-07-04 14:26:33 +0200

    Update bld.bat

--- conda/libclang/bld.bat
+++ conda/libclang/bld.bat
@@ -12,14 +12,6 @@ cd ..
 mkdir build
 cd build
 dir
-cmake  %srcdir%
-      -G "Visual Studio 9 2008"
-      -DBUILD_SHARED_LIBS=1
-      -DLLVM_ENABLE_RTTI=1
-      -DCMAKE_INSTALL_PREFIX=%PREFIX%
-      -DCMAKE_BUILD_TYPE=Release
-      -DCMAKE_C_COMPILER=%PREFIX%/bin/gcc
-      -DCMAKE_CXX_COMPILER=%PREFIX%/bin/g++
-      -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,%PREFIX%/lib -L%PREFIX%/lib"
+cmake -DBUILD_SHARED_LIBS=1 -DLLVM_ENABLE_RTTI=1 -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,%PREFIX%/lib -L%PREFIX%/lib" %srcdir%
 make
 make install
