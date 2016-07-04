6f21e4dd843dd032cb82a7472f27508816f7a953
Pierre Fernique <pfernique@gmail.com>
2016-07-04 11:07:13 +0200

    Update .travis.yml

--- conda/libclang/build.sh
+++ conda/libclang/build.sh
@@ -22,7 +22,7 @@ cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=1 \
                           -DCMAKE_BUILD_TYPE=Release \
                           -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
                           -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
-                          -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
+                          -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
                           $srcdir
 make
 make install
