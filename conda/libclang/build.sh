35c25c99ef6e8c7dd2a4af13f690ccbf69ec78b9
Pierre Fernique <pfernique@gmail.com>
2016-07-05 11:06:15 +0200

    Update build.sh

--- conda/libclang/build.sh
+++ conda/libclang/build.sh
@@ -32,6 +32,8 @@ else
                           -DLLVM_ENABLE_RTTI=1 \
                           -DCMAKE_INSTALL_PREFIX=${PREFIX} \
                           -DCMAKE_BUILD_TYPE=Release \
+                          -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
+                          -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
                           -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
                           $srcdir;
 fi
