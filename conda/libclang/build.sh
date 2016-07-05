d8b81caf56e3effd534ea97a8daa41b0a162fd61
Pierre Fernique <pfernique@gmail.com>
2016-07-05 09:34:09 +0200

    Update build.sh

--- conda/libclang/build.sh
+++ conda/libclang/build.sh
@@ -8,7 +8,8 @@ srcdir=`pwd`
 cd ..
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DLLVM_INCLUDE_TESTS=OFF \
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    cmake -G "Unix Makefiles" -DLLVM_INCLUDE_TESTS=OFF \
                           -DLLVM_INCLUDE_UTILS=OFF \
                           -DLLVM_INCLUDE_DOCS=OFF \
                           -DLLVM_INCLUDE_EXAMPLES=OFF \
@@ -20,6 +21,20 @@ cmake -G "Unix Makefiles" -DLLVM_INCLUDE_TESTS=OFF \
                           -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
                           -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
                           -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
-                          $srcdir
+                          $srcdir;
+fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    cmake -G "Unix Makefiles" -DLLVM_INCLUDE_TESTS=OFF \
+                          -DLLVM_INCLUDE_UTILS=OFF \
+                          -DLLVM_INCLUDE_DOCS=OFF \
+                          -DLLVM_INCLUDE_EXAMPLES=OFF \
+                          -DLLVM_ENABLE_TERMINFO=OFF \
+                          -DBUILD_SHARED_LIBS=1 \
+                          -DLLVM_ENABLE_RTTI=1 \
+                          -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+                          -DCMAKE_BUILD_TYPE=Release \
+                          -DCMAKE_CXX_LINK_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+                          $srcdir;
+fi
 make -j6
 make install
