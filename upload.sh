612a9c90bbcdeeb6b6b4556363c338972d8c2cd4
Pierre Fernique <pfernique@gmail.com>
2016-07-08 14:31:05 +0200

    Update .travis.yml

--- upload.sh
+++ upload.sh
@@ -1,7 +1,10 @@
+set -e
+
 anaconda login
+
 cd conda
 for i in 'libclang' 'python-clanglite'; do
-    conda build ${i%%/} -c StatisKit -c salford_systems;
+    conda build ${i%%/} -c StatisKit -c salford_systems -c dan_blanchard;
     CONDA_FILE=`conda build ${i%%/} --output`;
     anaconda upload --force --user StatisKit ${CONDA_FILE%%};
 done
