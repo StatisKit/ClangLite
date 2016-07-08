8eb00cdfbd40be992788d22d4c9dc8abe2d29172
Pierre Fernique <pfernique@gmail.com>
2016-07-08 10:19:18 +0200

    Change libclang recipe

--- conda/libclang/build.sh
+++ conda/libclang/build.sh
@@ -1,3 +1,5 @@
+echo $LD_LIBRARY_PATH
+echo $LIBRARY_PATH
 git checkout release_38
 cd tools
 git clone https://github.com/llvm-mirror/clang.git
@@ -13,7 +15,6 @@ cmake -G "Unix Makefiles" -DLLVM_INCLUDE_TESTS=OFF \
                           -DLLVM_INCLUDE_DOCS=OFF \
                           -DLLVM_INCLUDE_EXAMPLES=OFF \
                           -DLLVM_ENABLE_TERMINFO=OFF \
-                          -DBUILD_SHARED_LIBS=1 \
                           -DLLVM_ENABLE_RTTI=1 \
                           -DCMAKE_INSTALL_PREFIX=${PREFIX} \
                           -DCMAKE_BUILD_TYPE=Release \
--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 anaconda login
 cd conda
-for i in 'python-clanglite'; do
+for i in 'libclang' 'python-clanglite'; do
 	conda build ${i%%/} -c StatisKit;
 	CONDA_FILE=`conda build ${i%%/} --output`;
     anaconda upload --user StatisKit ${CONDA_FILE%%};
