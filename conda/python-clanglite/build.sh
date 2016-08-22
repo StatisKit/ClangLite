a19330f9bed069f7d6195248032e3873ec4fced4
Pierre Fernique <pfernique@gmail.com>
2016-08-22 10:30:11 +0200

    Update build.sh

--- conda/python-clanglite/build.sh
+++ conda/python-clanglite/build.sh
@@ -1,2 +1,2 @@
-scons py --prefix=$PREFIX -j$(( $CPU_COUNT > 2 ? $CPU_COUNT - 1 : $CPU_COUNT )) debug=1
+scons py --prefix=$PREFIX -j$(( $CPU_COUNT > 2 ? $CPU_COUNT - 1 : $CPU_COUNT ))
 $PYTHON setup.py install
