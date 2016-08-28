ffa7bb61559b6097c3d7d595e2dbb8353f3abffa
Pierre Fernique <pfernique@gmail.com>
2016-08-28 11:41:18 +0200

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -1,9 +1,6 @@
 # Install dependencies (build, run & tests)
 conda install libclang libboost python-scons pyyaml python numpy -c statiskit
 
-# Build libclanglite recipe
-scons cpp
-
-# Build python-clanglite recipe
+# Build PyClangLite recipe
 scons py
 pip install -e .
