7f15eedad0d2b109366ea5314c29147dd12dde88
Pierre Fernique <pfernique@gmail.com>
2016-08-28 17:52:41 +0200

    Update build.sh

--- conda/python-clanglite/build.sh
+++ conda/python-clanglite/build.sh
@@ -1,2 +1,2 @@
-scons py --repo-dir=$RECIPE_DIR/../../ --prefix=$PREFIX -j$(( $CPU_COUNT > 2 ? $CPU_COUNT - 1 : $CPU_COUNT ))
+scons py -j$(( $CPU_COUNT > 2 ? $CPU_COUNT - 1 : $CPU_COUNT ))
 $PYTHON setup.py install
