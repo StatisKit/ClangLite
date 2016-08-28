cd4e339aa56b33c24d8b7f3bd6e16592df9b7e6a
Pierre Fernique <pfernique@gmail.com>
2016-08-28 15:34:18 +0200

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -2,6 +2,6 @@
 conda install libclang libboost python-scons pyyaml python numpy -c statiskit
 
 # Build PyClangLite recipe
-CPU_COUNT=`python -c "import multiprocessing; multiprocessing.cpu_count()"`
+CPU_COUNT=`python -c "import multiprocessing; print(multiprocessing.cpu_count())"`
 scons -j$(( $CPU_COUNT > 2 ? $CPU_COUNT - 1 : $CPU_COUNT ))
 pip install -e .
