d552987de1977ef09ad41e37beb5da4c999512ad
Pierre Fernique <pfernique@gmail.com>
2016-09-09 14:09:48 +0200

    Update _clanglite.py

--- src/py/clanglite/_clanglite.py
+++ src/py/clanglite/_clanglite.py
@@ -2,13 +2,13 @@ __all__ = []
 
 
 # Import Boost.Python module
-import clanglite.__clanglite
+import __clanglite
 
 # Resolve scopes
-clanglite.__clanglite.clang.LinkageSpecDecl.language_i_ds = clanglite.__clanglite.clang._linkage_spec_decl.language_i_ds
-clanglite.__clanglite.clang.Decl.kind = clanglite.__clanglite.clang._decl.kind
-clanglite.__clanglite.clang.TemplateArgument.arg_kind = clanglite.__clanglite.clang._template_argument.arg_kind
-clanglite.__clanglite.clang.BuiltinType.kind = clanglite.__clanglite.clang._builtin_type.kind
-clanglite.__clanglite.clang.Type.type_class = clanglite.__clanglite.clang._type.type_class
+__clanglite.clang.LinkageSpecDecl.language_i_ds = __clanglite.clang._linkage_spec_decl.language_i_ds
+__clanglite.clang.Decl.kind = __clanglite.clang._decl.kind
+__clanglite.clang.TemplateArgument.arg_kind = __clanglite.clang._template_argument.arg_kind
+__clanglite.clang.BuiltinType.kind = __clanglite.clang._builtin_type.kind
+__clanglite.clang.Type.type_class = __clanglite.clang._type.type_class
 
 
