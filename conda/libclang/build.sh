1ea7fa04508eddcc6d8fffdcc05fca9cbfe3a08c
Pierre Fernique <pfernique@gmail.com>
2016-09-29 16:12:32 +0200

    Update build.sh

--- conda/libclang/build.sh
+++ conda/libclang/build.sh
@@ -1,12 +1,4 @@
-git fetch origin
-git checkout -b release_38 origin/release_38
-cd tools
-git clone https://github.com/llvm-mirror/clang.git
-cd clang
-git fetch origin
-git checkout -b release_38 origin/release_38
-cd ../..
-SRC_DIR=`pwd`
+PWD_DIR=`pwd`
 cd ..
 BLD_DIR=`mkdir "build_llvmclang"`
 cd $BLD_DIR
@@ -19,8 +11,35 @@ cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=ON \
                           -DLLVM_ENABLE_RTTI=ON \
                           -DCMAKE_INSTALL_PREFIX=${PREFIX} \
                           -DCMAKE_BUILD_TYPE=Release \
-                          $SRC_DIR;
+                          -DLLVM_TARGETS_TO_BUILD=host \
+                          $PWD_DIR;
 make -j$CPU_COUNT VERBOSE=1
 make install
 cd ..
 rm -fr $BLD_DIR
+# git fetch origin
+# git checkout -b release_38 origin/release_38
+# cd tools
+# git clone https://github.com/llvm-mirror/clang.git
+# cd clang
+# git fetch origin
+# git checkout -b release_38 origin/release_38
+# cd ../..
+# SRC_DIR=`pwd`
+# cd ..
+# BLD_DIR=`mkdir "build_llvmclang"`
+# cd $BLD_DIR
+# cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=ON \
+#                           -DLLVM_INCLUDE_TESTS=OFF \
+#                           -DLLVM_INCLUDE_UTILS=OFF \
+#                           -DLLVM_INCLUDE_DOCS=OFF \
+#                           -DLLVM_INCLUDE_EXAMPLES=OFF \
+#                           -DLLVM_ENABLE_TERMINFO=OFF \
+#                           -DLLVM_ENABLE_RTTI=ON \
+#                           -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+#                           -DCMAKE_BUILD_TYPE=Release \
+#                           $SRC_DIR;
+# make -j$CPU_COUNT VERBOSE=1
+# make install
+# cd ..
+# rm -fr $BLD_DIR
