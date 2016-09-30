c572c250f51d3505441c9b775245fe581c272563
Pierre Fernique <pfernique@gmail.com>
2016-09-30 08:24:12 +0200

    Update build.sh

--- conda/libclang/build.sh
+++ conda/libclang/build.sh
@@ -1,3 +1,10 @@
+cd tools
+curl http://llvm.org/releases/3.8.0/cfe-3.8.1.src.tar.xz
+tar xpvf cfe-3.8.1.src.tar.xz -C cfe-3.8.1
+mv cfe-3.8.1/cfe-3.8.1.src clang
+rm cfe-3.8.1.src.tar.xz
+rm -rf cfe-3.8.1
+cd ..
 PWD_DIR=`pwd`
 cd ..
 BLD_DIR=`mkdir "build_llvmclang"`
