cb285c0254648af4851e90e6c19e8cc49eaaf0fd
Pierre Fernique <pfernique@gmail.com>
2016-10-04 11:07:26 +0200

    Update _clanglite.h

--- src/py/_clanglite.h
+++ src/py/_clanglite.h
@@ -14,8 +14,13 @@
 /*                                                                                */
 /**********************************************************************************/
 
+#ifndef __CLANGLITE_H
+#define __CLANGLITE_H
+
 #include <boost/python.hpp>
 #include <type_traits>
 #include <clanglite/tool.h>
 
 namespace autowig { template<class T> using HeldType = T*; }
+
+#endif
