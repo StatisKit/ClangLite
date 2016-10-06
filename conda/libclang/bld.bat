59a12ecf3c06a30877445d1b463288e0125d0d20
Pierre Fernique <pfernique@gmail.com>
2016-10-06 13:51:56 +0200

    Update windows recipe

--- conda/libclang/bld.bat
+++ conda/libclang/bld.bat
@@ -1,9 +1,18 @@
-mkdir build
-cd build
+cd tools
+curl -O http://llvm.org/releases/3.8.1/cfe-3.8.1.src.tar.xz
+:: tar -xJf cfe-3.8.1.src.tar.xz
+7z x cfe-3.8.1.src.tar.xz
+7z x cfe-3.8.1.src.tar
+move cfe-3.8.1.src clang
+del cfe-3.8.1.src.tar
+del cfe-3.8.1.src.tar.xz
+cd ..
+mkdir build_llvmclang
+cd build_llvmclang
 
 set BUILD_CONFIG=Release
 
-REM Configure step
+:: Configure step
 if "%ARCH%"=="32" (
      set CMAKE_GENERATOR=Visual Studio 12 2013
 ) else (
@@ -12,7 +21,7 @@ if "%ARCH%"=="32" (
 set CMAKE_GENERATOR_TOOLSET=v120_xp
 :: set CMAKE_GENERATOR=MinGW Makefiles
 
-REM Reduce build times and package size by removing unused stuff
+:: Reduce build times and package size by removing unused stuff
 set CMAKE_CUSTOM=-DLLVM_TARGETS_TO_BUILD=X86 ^
                  -DLLVM_INCLUDE_TESTS=OFF ^
                  -DLLVM_INCLUDE_UTILS=OFF ^
@@ -23,7 +32,7 @@ set CMAKE_CUSTOM=-DLLVM_TARGETS_TO_BUILD=X86 ^
 cmake -G "%CMAKE_GENERATOR%" -T "%CMAKE_GENERATOR_TOOLSET%" -DCMAKE_BUILD_TYPE="%BUILD_CONFIG%" -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %CMAKE_CUSTOM% %SRC_DIR%
 if errorlevel 1 exit 1
 ::cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE="%BUILD_CONFIG%" -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %CMAKE_CUSTOM% %SRC_DIR%
-if errorlevel 1 exit 1
+:: if errorlevel 1 exit 1
 
 REM Build step
 cmake --build . --config "%BUILD_CONFIG%"
@@ -31,4 +40,9 @@ if errorlevel 1 exit 1
 
 REM Install step
 cmake --build . --config "%BUILD_CONFIG%" --target install
+if errorlevel 1 exit 1
+
+cd ..
+if errorlevel 1 exit 1
+rmdir build_llvmclang /s /q
 if errorlevel 1 exit 1
