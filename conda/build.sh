30362c767fb03b8378f74f93e583e2c515a17c06
Pierre Fernique <pfernique@gmail.com>
2016-10-06 11:31:09 +0200

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -7,5 +7,5 @@ conda install python-pkgtk -c statiskit -c conda-forge
 export TOOLCHAIN=`pkgtk toolchain`
 
 for CONDA_RECIPE in libclang libclanglite python-clanglite; do
-  conda build $CONDA_RECIPE -c statiskit
+  conda build $CONDA_RECIPE -c statiskit -c conda-forge
 done
