80509143fbf7da9af7afe1e00f4253dc739a053d
Pierre Fernique <pfernique@gmail.com>
2016-10-07 10:22:41 +0200

    Update bld.bat

--- conda/bld.bat
+++ conda/bld.bat
@@ -6,13 +6,11 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cd PyClangLite/conda
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-conda install python-pkgtk -c statiskit -c conda-forge
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-git clone https://gist.github.com/c491cb08d570beeba2c417826a50a9c3.git config-toolchain
-cd config-toolchain
+git clone https://gist.github.com/c491cb08d570beeba2c417826a50a9c3.git toolchain
+cd toolchain
 call config.bat
 cd ..
+rmdir toolchain /s /q
 
 conda build libclang -c statiskit
 if %errorlevel% neq 0 exit /b %errorlevel%
