46f62e6c8b68089fdbc10483b52a71a8ca880066
Pierre Fernique <pfernique@gmail.com>
2016-10-07 10:24:25 +0200

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -7,6 +7,7 @@ git clone https://gist.github.com/c491cb08d570beeba2c417826a50a9c3.git toolchain
 cd toolchain
 eval config.sh
 cd ..
+rm -rf toolchain
 
 for CONDA_RECIPE in libclang libclanglite python-clanglite; do
   conda build $CONDA_RECIPE -c statiskit
