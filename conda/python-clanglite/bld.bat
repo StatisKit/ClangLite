a85c4bad107f0c2601926027d085e65290b7058d
Pierre Fernique <pfernique@gmail.com>
2016-10-11 15:20:59 +0200

    Add Python 3 for windows

--- conda/python-clanglite/bld.bat
+++ conda/python-clanglite/bld.bat
@@ -1,2 +1,5 @@
+conda create -n py27 python=2
+activate py27
+conda install python-scons -c statiskit
 scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --toolchain=%TOOLCHAIN%
 python setup.py install
