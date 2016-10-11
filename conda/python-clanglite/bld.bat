f1ca49f1043011ff692b25c3f93b64f42453d1c8
Pierre Fernique <pfernique@gmail.com>
2016-10-11 16:34:28 +0200

    Update bld.bat

--- conda/python-clanglite/bld.bat
+++ conda/python-clanglite/bld.bat
@@ -2,4 +2,5 @@ conda create -n py27 python=2
 activate py27
 conda install python-scons -c statiskit
 scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --toolchain=%TOOLCHAIN%
+deactivate py27
 python setup.py install
