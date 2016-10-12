dd7f9d332bf33c42594dfe85a1bdc38d45bb734f
Pierre Fernique <pfernique@gmail.com>
2016-10-12 12:52:16 +0200

    Update bld.bat

--- conda/libclanglite/bld.bat
+++ conda/libclanglite/bld.bat
@@ -1,4 +1,8 @@
 conda create -n py27 python=2
+if errorlevel 1 exit 1
 activate py27
+if errorlevel 1 exit 1
 conda install python-scons -c statiskit
+if errorlevel 1 exit 1
 scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --toolchain=%TOOLCHAIN%
+if errorlevel 1 exit 1
