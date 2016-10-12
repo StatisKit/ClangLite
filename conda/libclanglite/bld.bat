c461b2ab7652355c9a92217117ff0421dc66b85c
Pierre Fernique <pfernique@gmail.com>
2016-10-12 14:31:43 +0200

    Update bld.bat

--- conda/libclanglite/bld.bat
+++ conda/libclanglite/bld.bat
@@ -1,2 +1,4 @@
-scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --toolchain=%TOOLCHAIN%
+pip install enscons
+if errorlevel 1 exit 1
+python -m SCons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --toolchain=%TOOLCHAIN%
 if errorlevel 1 exit 1
