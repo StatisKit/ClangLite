9497b61ac6fc7e7298766e65e6cc28cdb2b3f0dc
Pierre Fernique <pfernique@gmail.com>
2016-10-19 16:50:36 +0200

    Update bld.bat

--- conda/libclanglite/bld.bat
+++ conda/libclanglite/bld.bat
@@ -1,5 +1,6 @@
 dir "C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0"
 dir "C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\um"
+dir "C:\Program Files (x86)\Windows Kits\10\lib\10.0.14393.0\um\x64"
 
 scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --toolchain=%TOOLCHAIN%
 if errorlevel 1 exit 1
