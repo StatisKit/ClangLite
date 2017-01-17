448044e0446e8818de93abd110f7a364a5b2e455
Pierre Fernique <pfernique@gmail.com>
2017-01-17 16:09:40 +0100

    Update recipes

--- conda/python-clanglite/bld.bat
+++
@@ -1,6 +0,0 @@
-conda create -n py27 python=2
-activate py27
-conda install python-scons -c statiskit
-scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --toolchain=%TOOLCHAIN%
-deactivate py27
-python setup.py install
