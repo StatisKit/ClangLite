2a811bf79f60b08e12ceb6484974c9c6d50248d0
Pierre Fernique <pfernique@gmail.com>
2017-02-08 09:19:42 +0100

    Change MSVC 12.0 to 14.0

--- conda/libclanglite/bld.bat
+++ conda/libclanglite/bld.bat
@@ -1,6 +1,6 @@
 echo ON
 
-scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH% --msvc-version=12.0
+scons cpp --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH% --msvc-version=14.0
 if errorlevel 1 exit 1
 
 echo OFF
