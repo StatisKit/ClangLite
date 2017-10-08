8919a6e4ed9e912a354a0b0d064c819322ecaf79
Pierre Fernique <pfernique@gmail.com>
2017-10-08 22:14:48 +0200

    Update bld.bat

--- bin/conda/python-clanglite/bld.bat
+++ bin/conda/python-clanglite/bld.bat
@@ -5,9 +5,12 @@ if errorlevel 1 exit 1
 
 
 scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT%
+echo "SCONS DONE"
 if errorlevel 1 exit 1
 move %SP_DIR%\clanglite\__clanglite.pyd %SRC_DIR%\src\py\clanglite\__clanglite.pyd /Y
+echo "MOVE DONE"
 if errorlevel 1 exit 1
 %PYTHON% setup.py install --prefix=%PREFIX%
+echo "SETUP DONE"
 if errorlevel 1 exit 1
 echo OFF
