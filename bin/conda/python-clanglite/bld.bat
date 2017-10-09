eadc0e543eef80f128c5103235c852054a332aa0
Pierre Fernique <pfernique@gmail.com>
2017-10-09 05:45:01 +0200

    Update bld.bat

--- bin/conda/python-clanglite/bld.bat
+++ bin/conda/python-clanglite/bld.bat
@@ -3,16 +3,10 @@ echo ON
 if "%PY3K%"=="1" 2to3 -n -w %SRC_DIR%\src\py\clanglite
 if errorlevel 1 exit 1
 
-echo "SCONS START"
-scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT%
-echo "SCONS DONE"
+call scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT%
 if errorlevel 1 exit 1
-echo "MOVE START"
 move %SP_DIR%\clanglite\__clanglite.pyd %SRC_DIR%\src\py\clanglite\__clanglite.pyd
-echo "MOVE DONE"
 if errorlevel 1 exit 1
-echo "SETUP START"
 %PYTHON% setup.py install --prefix=%PREFIX%
-echo "SETUP DONE"
 if errorlevel 1 exit 1
 echo OFF
