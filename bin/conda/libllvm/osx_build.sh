5d628f3513a59ee7f9155b8ea3009629d714b949
Pierre Fernique <pfernique@gmail.com>
2017-11-14 07:20:58 +0100

    Add libllvm and libllvm-dev

---
+++ bin/conda/libllvm/osx_build.sh
@@ -0,0 +1,24 @@
+set -ve
+
+mkdir build_llvm
+cd build_llvm
+if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    # OSX needs 10.7 or above with libc++ enabled
+    export MACOSX_DEPLOYMENT_TARGET=10.9
+fi
+
+cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=ON \
+                          -DLLVM_INCLUDE_TESTS=OFF \
+                          -DLLVM_INCLUDE_UTILS=OFF \
+                          -DLLVM_INCLUDE_DOCS=OFF \
+                          -DLLVM_INCLUDE_EXAMPLES=OFF \
+                          -DLLVM_ENABLE_TERMINFO=OFF \
+                          -DLLVM_ENABLE_RTTI=ON \
+                          -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+                          -DCMAKE_BUILD_TYPE=Release \
+                          -DLLVM_TARGETS_TO_BUILD=host \
+                          $SRC_DIR;
+make -j$CPU_COUNT VERBOSE=1
+make install
+
+set +ve
