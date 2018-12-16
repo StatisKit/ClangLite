942501ec48da1f2ea22f714f2ace88fa396ee1e5
Pierre Fernique <pfernique@gmail.com>
2018-12-16 19:16:57 +0100

    Update 'clang' recipe [skip ci]

--- etc/conda/clang/build.sh
+++ etc/conda/clang/build.sh
@@ -17,20 +17,23 @@ cd build_clang
 # unset DEBUG_CFLAGS
 # unset DEBUG_CXXFLAGS
 
-# $GCC -x c++ -E -v /dev/null &> config.txt
-# export CFLAGS=`python $RECIPE_DIR/flags.py`
-# TMP_C_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
-# echo $TMP_C_LINK_FLAGS
+${GCC} -x c++ -E -v /dev/null &> config.txt
+export CFLAGS=`python $RECIPE_DIR/flags.py`
+TMP_C_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
+echo $TMP_C_LINK_FLAGS
 
-# $GXX -x c++ -E -v /dev/null &> config.txt
-# export CXXFLAGS=`python $RECIPE_DIR/flags.py`
-# TMP_CXX_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
-# echo $TMP_CXX_LINK_FLAGS
+${GXX} -x c++ -E -v /dev/null &> config.txt
+export CXXFLAGS=`python $RECIPE_DIR/flags.py`
+TMP_CXX_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
+echo $TMP_CXX_LINK_FLAGS
 
 cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=${GCC} \
                           -DCMAKE_CXX_COMPILER=${GXX} \
                           -DCMAKE_LINKER=${LD} \
                           -DLLVM_USE_LINKER={$LD} \
+                          -DCMAKE_EXE_LINKER_FLAGS="${TMP_C_LINK_FLAGS}" \
+                          -DCMAKE_C_LINK_FLAGS="${TMP_C_LINK_FLAGS}" \
+                          -DCMAKE_CXX_LINK_FLAGS="${TMP_CXX_LINK_FLAGS}" \
                           -DBUILD_SHARED_LIBS=ON \
                           -DLLVM_INCLUDE_TESTS=OFF \
                           -DLLVM_INCLUDE_UTILS=OFF \
@@ -41,12 +44,8 @@ cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=${GCC} \
                           -DCMAKE_INSTALL_PREFIX=${PREFIX} \
                           -DCMAKE_BUILD_TYPE=Release \
                           -DLLVM_TARGETS_TO_BUILD=host \
-                          $SRC_DIR;
+                          ..;
 make -j$CPU_COUNT VERBOSE=1
 make install
 
 set +ve
-
-#                          -DCMAKE_EXE_LINKER_FLAGS="${TMP_C_LINK_FLAGS}" \
-#                          -DCMAKE_C_LINK_FLAGS="${TMP_C_LINK_FLAGS}" \
-#                          -DCMAKE_CXX_LINK_FLAGS="${TMP_CXX_LINK_FLAGS}" \
