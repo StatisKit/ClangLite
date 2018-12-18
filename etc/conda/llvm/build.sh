259211aa9dacfebbe4ffc8abe9d02359948969b8
Pierre Fernique <pfernique@gmail.com>
2018-12-18 20:18:12 +0100

    Update 'llvm' recipe [skip ci]

--- etc/conda/llvm/build.sh
+++ etc/conda/llvm/build.sh
@@ -3,28 +3,28 @@ set -ve
 mkdir build
 cd build
 
-${GCC} -x c++ -E -v /dev/null &> config.txt
-export CFLAGS="$(python $RECIPE_DIR/flags.py) ${CFLAGS}"
-TMP_C_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
+# ${GCC} -x c++ -E -v /dev/null &> config.txt
+# export CFLAGS="$(python $RECIPE_DIR/flags.py) ${CFLAGS}"
+# TMP_C_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
 
-TMP_C_LINK_FLAGS="${TMP_CXX_LINK_FLAGS} -L${PREFIX}/lib"
-echo ${TMP_C_LINK_FLAGS}
+# TMP_C_LINK_FLAGS="${TMP_CXX_LINK_FLAGS} -L${PREFIX}/lib"
+# echo ${TMP_C_LINK_FLAGS}
 
-${GXX} -x c++ -E -v /dev/null &> config.txt
-export CXXFLAGS="$(python $RECIPE_DIR/flags.py) ${CXXFLAGS}"
-TMP_CXX_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
+# ${GXX} -x c++ -E -v /dev/null &> config.txt
+# export CXXFLAGS="$(python $RECIPE_DIR/flags.py) ${CXXFLAGS}"
+# TMP_CXX_LINK_FLAGS=`python $RECIPE_DIR/link_flags.py`
 
-TMP_CXX_LINK_FLAGS="${TMP_CXX_LINK_FLAGS} -L${PREFIX}/lib"
-echo ${TMP_CXX_LINK_FLAGS}
+# TMP_CXX_LINK_FLAGS="${TMP_CXX_LINK_FLAGS} -L${PREFIX}/lib"
+# echo ${TMP_CXX_LINK_FLAGS}
 
-cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=${GCC} \
-                          -DCMAKE_CXX_COMPILER=${GXX} \
+export LDFLAGS=${LDFLAGS}" -L${PREFIX}/lib"
+export CFLAGS=${CFLAGS}" -I${PREFIX}/include"
+export CXXFLAGS=${CXXFLAGS}" -I${PREFIX}/include"
+
+cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=${CC} \
+                          -DCMAKE_CXX_COMPILER=${CXX} \
                           -DCMAKE_LINKER=${LD} \
-                          -DLLVM_USE_LINKER={$LD} \
-                          -DLLVM_USE_HOST_TOOLS=ON \
-                          -DCMAKE_EXE_LINKER_FLAGS="${TMP_C_LINK_FLAGS}" \
-                          -DCMAKE_C_LINK_FLAGS="${TMP_C_LINK_FLAGS}" \
-                          -DCMAKE_CXX_LINK_FLAGS="${TMP_CXX_LINK_FLAGS}" \
+                          -DLLVM_USE_LINKER=${LD} \
                           -DBUILD_SHARED_LIBS=ON \
                           -DLLVM_INCLUDE_TESTS=OFF \
                           -DLLVM_INCLUDE_UTILS=OFF \
