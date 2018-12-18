1f06b998d3ed2f6aa3894cddcc62004d4dfb5678
Pierre Fernique <pfernique@gmail.com>
2018-12-18 15:34:20 +0100

    Update 'clang' recipe [skip ci]

--- etc/conda/clang/build.sh
+++ etc/conda/clang/build.sh
@@ -12,15 +12,20 @@ cd build
 ${GCC} -x c++ -E -v /dev/null &> config.txt
 export CFLAGS="$(python $RECIPE_DIR/flags.py) ${CFLAGS}"
 TMP_C_LINK_FLAGS="$(python $RECIPE_DIR/link_flags.py)"
+echo ${TMP_C_LINK_FLAGS}
 
 ${GXX} -x c++ -E -v /dev/null &> config.txt
 export CXXFLAGS="$(python $RECIPE_DIR/flags.py) ${CXXFLAGS}"
 TMP_CXX_LINK_FLAGS="$(python $RECIPE_DIR/link_flags.py)"
+echo ${TMP_C_LINK_FLAGS}
 
 cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=${GCC} \
                           -DCMAKE_CXX_COMPILER=${GXX} \
                           -DCMAKE_LINKER=${LD} \
                           -DLLVM_USE_LINKER={$LD} \
+                          -DCMAKE_EXE_LINKER_FLAGS="${TMP_C_LINK_FLAGS}" \
+                          -DCMAKE_C_LINK_FLAGS="${TMP_C_LINK_FLAGS}" \
+                          -DCMAKE_CXX_LINK_FLAGS="${TMP_CXX_LINK_FLAGS}" \
                           -DBUILD_SHARED_LIBS=ON \
                           -DLLVM_INCLUDE_TESTS=OFF \
                           -DLLVM_INCLUDE_UTILS=OFF \
