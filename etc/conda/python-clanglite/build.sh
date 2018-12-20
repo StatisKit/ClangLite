3692723238f3206e32e4f70b23d966604ecf275d
Pierre Fernique <pfernique@gmail.com>
2018-12-20 07:27:20 +0100

    Correct 'python-clanglite' recipe [skip ci]

--- etc/conda/python-clanglite/build.sh
+++ etc/conda/python-clanglite/build.sh
@@ -24,10 +24,10 @@
 set -ev
 
 if [[ "$PY3K" = "1" ]]; then
-    2to3 -n -w $SRC_DIR/src/py/clanglite
+    2to3 -n -w ${SRC_DIR}/src/py/clanglite
 fi
 
-scons py --prefix=$PREFIX -j$CPU_COUNT
-python setup.py install --prefix=$PREFIX
+scons py --prefix=${PREFIX} -j$CPU_COUNT
+${PYTHON} setup.py install --prefix=$PREFIX
 
 set +ev
